b17fefc0950f995b5be7f2d78c828753246f61b4
Viktor Morhun <morhun.fe@gmail.com>
2024-08-06 15:38:27 +0300

    change naming to camelCase

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 'use strict'
 
-//Task 1.1
-const AGE_BUTTON = document.getElementById("age-submit");
-AGE_BUTTON.addEventListener("click", determineAgeGroup);
+// Task 1.1
+const ageButton = document.getElementById("age-submit");
+ageButton.addEventListener("click", determineAgeGroup);
 
 function determineAgeGroup() {
-  let USER_AGE = document.getElementById("age").value;
+  let userAge = document.getElementById("age").value;
   let userGroup = "";
 
-  if (!USER_AGE || Number(USER_AGE) > 130 || Number(USER_AGE) < 0) {
+  if (!userAge || Number(userAge) > 130 || Number(userAge) < 0) {
     userGroup = 'Please, enter your age (from 0 to 130).';
   } else {
-    USER_AGE = Number(USER_AGE);
+    userAge = Number(userAge);
 
     switch (true) {
-      case USER_AGE >= 0 && USER_AGE <= 11:
+      case userAge >= 0 && userAge <= 11:
         userGroup = "You are a child ;)";
         break;
-      case USER_AGE > 11 && USER_AGE < 18:
+      case userAge > 11 && userAge < 18:
         userGroup = "You are a teenager.";
         break;
-      case USER_AGE >= 18 && USER_AGE < 60:
+      case userAge >= 18 && userAge < 60:
         userGroup = "You are an adult.";
         break;
-      case USER_AGE > 60:
+      case userAge > 60:
         userGroup = "You are a senior.";
         break;
       default:
@@ -34,145 +34,138 @@ function determineAgeGroup() {
   document.getElementById("age-result").innerHTML = userGroup;
 }
 
-
-//Task 1.2
-const SYMBOL_BUTTON = document.getElementById("number-submit");
-SYMBOL_BUTTON.addEventListener("click", findSymbol);
+// Task 1.2
+const symbolButton = document.getElementById("number-submit");
+symbolButton.addEventListener("click", findSymbol);
 
 function findSymbol() {
-  const USER_NUMBER = Number(document.querySelector("#number").value);
-  const SYMBOL_LIST = [")", "!", "@", "#", "$", "%", "^", "&", "*", "("];
+  const userNumber = Number(document.querySelector("#number").value);
+  const symbolList = [")", "!", "@", "#", "$", "%", "^", "&", "*", "("];
 
-  if (USER_NUMBER < 0 || USER_NUMBER > 9 || !document.querySelector("#number").value) {
-    document.getElementById("number-result").innerHTML = "Enter a number in range 0 to 9." 
+  if (userNumber < 0 || userNumber > 9 || !document.querySelector("#number").value) {
+    document.getElementById("number-result").innerHTML = "Enter a number in range 0 to 9.";
   } else {
-    document.getElementById("number-result").innerHTML = `Your symbol is ${SYMBOL_LIST[USER_NUMBER]}`;
+    document.getElementById("number-result").innerHTML = `Your symbol is ${symbolList[userNumber]}`;
   }
 }
 
-
-//Task 1.3
-const RANGE_BUTTON = document.getElementById('range-submit');
-RANGE_BUTTON.addEventListener("click", calculateSum);
+// Task 1.3
+const rangeButton = document.getElementById('range-submit');
+rangeButton.addEventListener("click", calculateSum);
 
 function calculateSum() {
   if (!document.querySelector("#range-start").value || !document.querySelector("#range-end").value) {
     document.getElementById("range-result").innerHTML = 'Please enter two numbers.';  
   } else {
-    const FIRST_NUMBER = parseInt(document.querySelector("#range-start").value);
-    const SECOND_NUMBER = parseInt(document.querySelector("#range-end").value);
+    const firstNumber = parseInt(document.querySelector("#range-start").value);
+    const secondNumber = parseInt(document.querySelector("#range-end").value);
     let result = 0;
 
-    if (FIRST_NUMBER > SECOND_NUMBER) {
-      document.getElementById("range-result").innerHTML = "The first number must be less than the second."
+    if (firstNumber > secondNumber) {
+      document.getElementById("range-result").innerHTML = "The first number must be less than the second.";
     } else {
-      result = (FIRST_NUMBER + SECOND_NUMBER) * (SECOND_NUMBER - FIRST_NUMBER + 1) / 2; 
-      document.getElementById("range-result").innerHTML = `Sum of all numbers from ${FIRST_NUMBER} to ${SECOND_NUMBER} is equal to ${result}`;  
+      result = (firstNumber + secondNumber) * (secondNumber - firstNumber + 1) / 2; 
+      document.getElementById("range-result").innerHTML = `Sum of all numbers from ${firstNumber} to ${secondNumber} is equal to ${result}`;  
     }
   }
-  
 }
 
-
-//Task 1.4
-const GCD_BUTTON = document.getElementById('gcd-submit');
-GCD_BUTTON.addEventListener('click', findGCD);
+// Task 1.4
+const gcdButton = document.getElementById('gcd-submit');
+gcdButton.addEventListener('click', findGCD);
 
 function findGCD() {
-  const FIRST_NUMBER = Number(document.getElementById('gcd-number1').value);
-  const SECOND_NUMBER = Number(document.getElementById('gcd-number2').value);
+  const firstNumber = Number(document.getElementById('gcd-number1').value);
+  const secondNumber = Number(document.getElementById('gcd-number2').value);
 
-  if (FIRST_NUMBER <= 0 || SECOND_NUMBER <= 0 || FIRST_NUMBER > 999999999999999 || SECOND_NUMBER > 999999999999999) {
+  if (firstNumber <= 0 || secondNumber <= 0 || firstNumber > 999999999999999 || secondNumber > 999999999999999) {
     document.getElementById('gcd-result').innerHTML = `Please enter two positive numbers in range 1 to 999999999999999`;
   } else {
-    const NUMBERS = FIRST_NUMBER > SECOND_NUMBER 
-    ? [SECOND_NUMBER, FIRST_NUMBER]
-    : [FIRST_NUMBER, SECOND_NUMBER];
+    const numbers = firstNumber > secondNumber 
+    ? [secondNumber, firstNumber]
+    : [firstNumber, secondNumber];
   
     document.getElementById('gcd-result').innerHTML = `Calculating...`;
 
-    for (let i = NUMBERS[0]; i > 0; i--) {
-      if (NUMBERS[1] % i === 0 && NUMBERS[0] % i === 0) {
-        document.getElementById('gcd-result').innerHTML = `GCD of the giving numbers(${FIRST_NUMBER}, ${SECOND_NUMBER}) is: ${i}`;
+    for (let i = numbers[0]; i > 0; i--) {
+      if (numbers[1] % i === 0 && numbers[0] % i === 0) {
+        document.getElementById('gcd-result').innerHTML = `GCD of the giving numbers(${firstNumber}, ${secondNumber}) is: ${i}`;
         break;
       }
     }  
   }
 }
 
-
-//Task 1.5
-const DIVISORS_BUTTON = document.getElementById('divisor-submit');
-DIVISORS_BUTTON.addEventListener('click', findDivisors);
+// Task 1.5
+const divisorsButton = document.getElementById('divisor-submit');
+divisorsButton.addEventListener('click', findDivisors);
 
 function findDivisors() {
-  const DIVIDED_NUMBER = Number(document.getElementById('divisor-number').value);
+  const dividedNumber = Number(document.getElementById('divisor-number').value);
 
-  if (DIVIDED_NUMBER <= 0) {
-    document.getElementById('divisor-result').innerHTML = 'Please enter a positive number.'
+  if (dividedNumber <= 0) {
+    document.getElementById('divisor-result').innerHTML = 'Please enter a positive number.';
   } else {
     let divisorsArr = [];
 
-    for(let i = 1; i <= Math.sqrt(DIVIDED_NUMBER); i++) {
-      if (DIVIDED_NUMBER % i === 0) {
-        if (DIVIDED_NUMBER / i === i) {
+    for(let i = 1; i <= Math.sqrt(dividedNumber); i++) {
+      if (dividedNumber % i === 0) {
+        if (dividedNumber / i === i) {
           divisorsArr.push(i);
         } else {
-          divisorsArr.push(i, DIVIDED_NUMBER/i);
+          divisorsArr.push(i, dividedNumber/i);
         }
       }
     }
   
-    document.getElementById('divisor-result').innerHTML = `Divisors of ${DIVIDED_NUMBER} are: ${divisorsArr.sort((a, b) => a - b).join(', ')}.`;
+    document.getElementById('divisor-result').innerHTML = `Divisors of ${dividedNumber} are: ${divisorsArr.sort((a, b) => a - b).join(', ')}.`;
   }
 }
 
-
-//Task 2.1
-const PALINDROME_BUTTON = document.getElementById('palindrome-submit');
-PALINDROME_BUTTON.addEventListener('click', checkPalindrome);
+// Task 2.1
+const palindromeButton = document.getElementById('palindrome-submit');
+palindromeButton.addEventListener('click', checkPalindrome);
 
 function checkPalindrome() {
-  const PALINDROME_NUMBER = document.getElementById('palindrome-number').value;
+  const palindromeNumber = document.getElementById('palindrome-number').value;
 
-  if (Number(PALINDROME_NUMBER) <= 0) {
+  if (Number(palindromeNumber) <= 0) {
     document.getElementById('palindrome-result').innerHTML = 'Please enter a positive number.';
   } else {
-    if (PALINDROME_NUMBER.split('').reverse().join('') === PALINDROME_NUMBER && PALINDROME_NUMBER.length > 1) {
-      document.getElementById('palindrome-result').innerHTML = `Yes, ${PALINDROME_NUMBER} is a palindrome.`;
+    if (palindromeNumber.split('').reverse().join('') === palindromeNumber && palindromeNumber.length > 1) {
+      document.getElementById('palindrome-result').innerHTML = `Yes, ${palindromeNumber} is a palindrome.`;
     } else {
-      document.getElementById('palindrome-result').innerHTML = `No, ${PALINDROME_NUMBER} isn't a palindrome.`;
+      document.getElementById('palindrome-result').innerHTML = `No, ${palindromeNumber} isn't a palindrome.`;
     }
   }
 }
 
-
-//Task 2.2
-const DISCOUNT_BUTTON = document.getElementById('discount-submit');
-DISCOUNT_BUTTON.addEventListener('click', calculateDiscount);
+// Task 2.2
+const discountButton = document.getElementById('discount-submit');
+discountButton.addEventListener('click', calculateDiscount);
 
 function calculateDiscount() {
-  const PURCHASE_AMOUNT = Number(document.getElementById('purchase-amount').value);
+  const purchaseAmount = Number(document.getElementById('purchase-amount').value);
   let discount = '';
 
-  if (PURCHASE_AMOUNT <= 0) {
+  if (purchaseAmount <= 0) {
     discount = 'Please enter a purchase amount (minimum value is 1 eur)';
   } else {
     switch (true) {
-      case PURCHASE_AMOUNT < 200:
-        discount = `There is no discount. Total to be paid ${PURCHASE_AMOUNT} eur. Mininmum amount for discount is 300 eur.`;
+      case purchaseAmount < 200:
+        discount = `There is no discount. Total to be paid ${purchaseAmount} eur. Mininmum amount for discount is 300 eur.`;
         break;
   
-      case PURCHASE_AMOUNT < 300:
-        discount = `Your discount is 3%. Total to be paid ${(PURCHASE_AMOUNT * 0.97).toFixed(2)} eur.`;
+      case purchaseAmount < 300:
+        discount = `Your discount is 3%. Total to be paid ${(purchaseAmount * 0.97).toFixed(2)} eur.`;
         break;
   
-      case PURCHASE_AMOUNT < 500:
-        discount = `Your discount is 5%. Total to be paid ${(PURCHASE_AMOUNT * 0.95).toFixed(2)} eur.`;
+      case purchaseAmount < 500:
+        discount = `Your discount is 5%. Total to be paid ${(purchaseAmount * 0.95).toFixed(2)} eur.`;
         break;
   
-      case PURCHASE_AMOUNT >= 500:
-        discount = `Your discount is 7%. Total to be paid ${(PURCHASE_AMOUNT * 0.93).toFixed(2)} eur.`;
+      case purchaseAmount >= 500:
+        discount = `Your discount is 7%. Total to be paid ${(purchaseAmount * 0.93).toFixed(2)} eur.`;
         break;
     }
   }
@@ -180,20 +173,19 @@ function calculateDiscount() {
   document.getElementById('discount-result').innerHTML = discount;
 }
 
-//Task 2.3
-const STATISTICS_BUTTON = document.getElementById('statistics-submit');
-STATISTICS_BUTTON.addEventListener('click', statisticNumbers);
-
+// Task 2.3
+const statisticsButton = document.getElementById('statistics-submit');
+statisticsButton.addEventListener('click', statisticNumbers);
 
 function statisticNumbers() {
-  const USER_NUMBERS = document.getElementById('statistics-number').value.trim();
+  const userNumbers = document.getElementById('statistics-number').value.trim();
   let evenCounter = 0;
   let oddCounter = 0;
   let positiveCounter = 0;
   let negativeCounter = 0;
 
-  if (USER_NUMBERS.match(/^-?\d+(,-?\d+)*$/)) {
-    USER_NUMBERS.split(',').forEach(element => {
+  if (userNumbers.match(/^-?\d+(,-?\d+)*$/)) {
+    userNumbers.split(',').forEach(element => {
       if (Number(element) % 2 === 0) {
         evenCounter++;
       } else {
@@ -204,7 +196,7 @@ function statisticNumbers() {
       } else if (Number(element) < 0) {
         negativeCounter++;
       }
-    });;
+    });
 
     document.getElementById('statistics-result').innerHTML = `Even: ${evenCounter}, odd: ${oddCounter}, positive: ${positiveCounter}, negative: ${negativeCounter}, zero: ${evenCounter + oddCounter - positiveCounter - negativeCounter}`;
   } else {
@@ -212,126 +204,119 @@ function statisticNumbers() {
   }
 }
 
-
-//Task 2.4
-const DAY_BUTTON = document.getElementById('weekday-submit');
+// Task 2.4
+const dayButton = document.getElementById('weekday-submit');
 let dayCounter = 0;
-DAY_BUTTON.addEventListener('click', dayRoll);
+dayButton.addEventListener('click', dayRoll);
 
 function dayRoll() {
-  const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  document.getElementById('weekday-result').innerHTML = `${DAYS[dayCounter]}. Want to see the next day?`;
+  document.getElementById('weekday-result').innerHTML = `${days[dayCounter]}. Want to see the next day?`;
   dayCounter = dayCounter === 6 ? 0 : ++dayCounter;
 }
 
-
-//Task 3.1
-const GUESS_START = document.getElementById('guess-start');
-GUESS_START.addEventListener('click', startGuess);
+// Task 3.1
+const guessStart = document.getElementById('guess-start');
+guessStart.addEventListener('click', startGuess);
 
 function startGuess() {
-  const DECREASE_BUTTON = document.getElementById('less-number');
-  const INCREASE_BUTTON = document.getElementById('bigger-number');
-  const RESET_BUTTON = document.getElementById('block-refresh');
-  const GUESS_END = document.getElementById('guess-end');
-  const GUESS_RESULT = document.getElementById('guess-result');
+  const decreaseButton = document.getElementById('less-number');
+  const increaseButton = document.getElementById('bigger-number');
+  const resetButton = document.getElementById('block-refresh');
+  const guessEnd = document.getElementById('guess-end');
+  const guessResult = document.getElementById('guess-result');
 
-  GUESS_START.classList.add('hide');
-  DECREASE_BUTTON.classList.remove('hide');
-  GUESS_END.classList.remove('hide');
-  INCREASE_BUTTON.classList.remove('hide');
+  guessStart.classList.add('hide');
+  decreaseButton.classList.remove('hide');
+  guessEnd.classList.remove('hide');
+  increaseButton.classList.remove('hide');
 
-  DECREASE_BUTTON.addEventListener('click', lowerHalf);
-  INCREASE_BUTTON.addEventListener('click', upperHalf);
-  GUESS_END.addEventListener('click', () => endGuess(`Your number is ${midNumber}`));
-  RESET_BUTTON.addEventListener('click', () => endGuess(''));
+  decreaseButton.addEventListener('click', lowerHalf);
+  increaseButton.addEventListener('click', upperHalf);
+  guessEnd.addEventListener('click', () => endGuess(`Your number is ${midNumber}`));
+  resetButton.addEventListener('click', () => endGuess(''));
 
   let startIndex = 1;
   let endIndex = 100;
-  let midNumber = Math.floor((endIndex + startIndex)/2);
-  GUESS_RESULT.innerHTML = `Is your number ${midNumber}?`;
-
+  let midNumber = Math.floor((endIndex + startIndex) / 2);
+  guessResult.innerHTML = `Is your number ${midNumber}?`;
 
   function lowerHalf() {
     endIndex = midNumber;
-    midNumber = Math.floor((endIndex + startIndex)/2);
-    GUESS_RESULT.innerHTML = `Is your number ${midNumber}?`;
+    midNumber = Math.floor((endIndex + startIndex) / 2);
+    guessResult.innerHTML = `Is your number ${midNumber}?`;
   }
 
   function upperHalf() {
     startIndex = midNumber;
-    midNumber = Math.floor((endIndex + startIndex)/2);
-    GUESS_RESULT.innerHTML = `Is your number ${midNumber}?`;
+    midNumber = Math.floor((endIndex + startIndex) / 2);
+    guessResult.innerHTML = `Is your number ${midNumber}?`;
   }
   
   function endGuess(output) { 
-    GUESS_START.classList.remove('hide');
-    DECREASE_BUTTON.classList.add('hide');
-    GUESS_END.classList.add('hide');
-    INCREASE_BUTTON.classList.add('hide');
+    guessStart.classList.remove('hide');
+    decreaseButton.classList.add('hide');
+    guessEnd.classList.add('hide');
+    increaseButton.classList.add('hide');
 
-    GUESS_RESULT.innerHTML = output;
+    guessResult.innerHTML = output;
   }
 }
 
-
-//Task 3.2
-const MULTIPLY_BUTTON = document.getElementById('multiplication-generate');
-MULTIPLY_BUTTON.addEventListener('click', generateTable);
+// Task 3.2
+const multiplyButton = document.getElementById('multiplication-generate');
+multiplyButton.addEventListener('click', generateTable);
 
 function generateTable() {
-  const ROOT_DIV = document.getElementById('multiplication-result');
-  ROOT_DIV.innerHTML = '';
+  const rootDiv = document.getElementById('multiplication-result');
+  rootDiv.innerHTML = '';
 
   for (let i = 1; i <= 9; i++) {
-    const INNER_DIV = document.createElement('div');
-    INNER_DIV.className = 'task__container';
+    const innerDiv = document.createElement('div');
+    innerDiv.className = 'task__container';
 
     for (let j = 1; j <= 10; j++) {
-      const INNER_SPAN = document.createElement('span');
-      INNER_SPAN.className = 'multiply__span';
-      INNER_SPAN.textContent = `${i} * ${j} = ${i*j}`;
+      const innerSpan = document.createElement('span');
+      innerSpan.className = 'multiply__span';
+      innerSpan.textContent = `${i} * ${j} = ${i * j}`;
 
-      INNER_DIV.appendChild(INNER_SPAN);
+      innerDiv.appendChild(innerSpan);
     }
 
-    ROOT_DIV.appendChild(INNER_DIV);
+    rootDiv.appendChild(innerDiv);
   }
 }
 
-
-//Task 3.3
-const DATE_BUTTON = document.getElementById('date-submit');
-DATE_BUTTON.addEventListener('click', nextDay);
+// Task 3.3
+const dateButton = document.getElementById('date-submit');
+dateButton.addEventListener('click', nextDay);
 
 function nextDay() {
-  const CURRENT_DATE = document.getElementById('user-date').value;
+  const currentDate = document.getElementById('user-date').value;
 
-  if (CURRENT_DATE) {
-    const DATE = new Date(CURRENT_DATE);
-    DATE.setDate(DATE.getDate() + 1);
-    const FORMATED_DATE = DATE.toString().split(' ').slice(1, 4).join(' ');
+  if (currentDate) {
+    const date = new Date(currentDate);
+    date.setDate(date.getDate() + 1);
+    const formattedDate = date.toString().split(' ').slice(1, 4).join(' ');
 
-    document.getElementById('date-result').innerHTML = `Next day is ${FORMATED_DATE}`;
+    document.getElementById('date-result').innerHTML = `Next day is ${formattedDate}`;
   } else {
     document.getElementById('date-result').innerHTML = 'Please enter the correct date.';
   }
 }
 
-
-//Reset button 
-const RESET_BUTTON = document.querySelectorAll('.reset__button:not(#block-refresh)');
-RESET_BUTTON.forEach(button => {
+// Reset button 
+const resetButton = document.querySelectorAll('.reset__button:not(#block-refresh)');
+resetButton.forEach(button => {
   button.addEventListener('click', () => resetBlock(button));
 });
 
-
 function resetBlock(element) {
-  const SECTION = element.closest('section');
-  const SECTION_INPUTS = SECTION.querySelectorAll('.task__input');
-  const SECTION_RESULTS = SECTION.querySelectorAll('.task__result');
+  const section = element.closest('section');
+  const sectionInputs = section.querySelectorAll('.task__input');
+  const sectionResults = section.querySelectorAll('.task__result');
 
-  SECTION_INPUTS.forEach(input => input.value = '');
-  SECTION_RESULTS.forEach(result => result.innerHTML = '');
+  sectionInputs.forEach(input => input.value = '');
+  sectionResults.forEach(result => result.innerHTML = '');
 }
